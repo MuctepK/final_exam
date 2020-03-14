b8d33aee8a907723b239f23899dfb3a73bfdb5f4
Kutman Mairambek uulu <muctep.k@gmail.com>
2020-03-14 10:19:49 +0600

    Создал модель Файл

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    name = models.CharField(verbose_name='Название файла', max_length=128)
+    author = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True, blank=True, related_name='files')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата добавления')
+    file = models.FileField(verbose_name='Файл', upload_to='files')
