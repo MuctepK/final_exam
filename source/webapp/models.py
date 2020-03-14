5382af252bdcf42a91c5fa9099f3591e3db8f10a
Kutman Mairambek uulu <muctep.k@gmail.com>
2020-03-14 10:57:03 +0600

    Добавил вывод всех файлов на главной странице

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -6,3 +6,6 @@ class File(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True, blank=True, related_name='files')
     created_at = models.DateField(auto_now_add=True, verbose_name='Дата добавления')
     file = models.FileField(verbose_name='Файл', upload_to='files')
+
+    def __str__(self):
+        return self.name
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from webapp.models import File
+
+
+class IndexView(ListView):
+    model = File
+    template_name = 'index.html'
+    context_object_name = 'files'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return File.objects.all().order_by('-created_at')
