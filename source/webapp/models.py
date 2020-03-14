f0f2bad196fba187c742899d89e934f75bad28cc
Kutman Mairambek uulu <muctep.k@gmail.com>
2020-03-14 11:53:22 +0600

    Добавлена пагинация

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class File(models.Model):
     name = models.CharField(verbose_name='Название файла', max_length=128)
     author = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True, blank=True, related_name='files')
-    created_at = models.DateField(auto_now_add=True, verbose_name='Дата добавления')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
     file = models.FileField(verbose_name='Файл', upload_to='files')
 
     def __str__(self):
