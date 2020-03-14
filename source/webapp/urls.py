22e91521f283fbf11767d0c64c1a218671fd830c
Kutman Mairambek uulu <muctep.k@gmail.com>
2020-03-14 11:23:02 +0600

    Добавлено создание файла

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from webapp.views import IndexView, FileDetailView
+from webapp.views import IndexView, FileDetailView, FileCreateView
 
 urlpatterns = [
    path('', IndexView.as_view(), name='index'),
-   path('<int:pk>/file', FileDetailView.as_view(), name='detail_file')
+   path('<int:pk>/file/', FileDetailView.as_view(), name='detail_file'),
+   path('create/', FileCreateView.as_view(), name='create_file'),
 ]
 
 app_name = 'webapp'
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView
 
+from webapp.forms import FileForm
 from webapp.models import File
 
 
@@ -19,3 +21,16 @@ class FileDetailView(DetailView):
     model = File
     context_object_name = 'file'
 
+
+class FileCreateView(CreateView):
+    template_name = 'file_create.html'
+    model = File
+    form_class = FileForm
+
+    def form_valid(self, form):
+        if self.request.user.is_authenticated:
+            form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("webapp:detail_file", kwargs={"pk": self.object.pk})
